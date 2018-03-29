8ef0b579881304c92e4046b560fd75745de82d7f
padmanabhanvj9 <padmanabhanvj9@gmail.com>
2018-03-29 15:21:03 +0530

    Update apptech.py

--- apptech.py
+++ apptech.py
@@ -4,17 +4,18 @@ This is the master file for all the Web services
 related to Doctor Appt application
 
 '''
-
+import json
 from flask import Flask,request, jsonify
+from HelloWorld import pad
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
    return "Hello World!"
-@app.route("/user/<data>")
-def helloName(data):
-   return data
+@app.route("/new")
+def new():
+   return pad(request)
 
 if __name__ == "__main__":
    app.run(debug=True)
